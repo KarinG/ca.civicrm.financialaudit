e65863b54b0c176f3345f09eb0ef09bee4c21d0c
KarinG <karin@semper-it.com>
2018-07-26 16:52:22 -0600

    First Functional Finacial audit.

--- CRM/Financialaudit/Form/Report/Financialaudit.php
+++ CRM/Financialaudit/Form/Report/Financialaudit.php
@@ -33,10 +33,9 @@
  *
  */
 class CRM_Financialaudit_Form_Report_Financialaudit extends CRM_Extendedreport_Form_Report_ExtendedReport {
-  protected $_customGroupExtends = array('Contribution');
+  // protected $_customGroupExtends = array('Contribution');
 
   protected $_baseTable = 'civicrm_line_item';
-
   protected $_aclTable = 'civicrm_contact';
 
   /**
@@ -44,9 +43,9 @@ class CRM_Financialaudit_Form_Report_Financialaudit extends CRM_Extendedreport_F
     */
   public function __construct() {
     $this->_columns
-      = $this->getColumns('Contact', array('order_by' => TRUE, 'fields_defaults' => array('sort_name', 'id')))
-      + $this->getColumns('Contribution', array('order_by' => TRUE, 'fields_defaults' => array('total_amount', 'trxn_id', 'receive_date', 'id', 'contribution_status_id')))
-      + $this->getColumns('LineItem', array('order_by' => TRUE, 'fields_defaults' => array('financial_type_id', 'line_total', 'tax_amount')));
+      = $this->getColumns('Contact', array('fields_defaults' => array('sort_name', 'id')))
+      + $this->getColumns('Contribution', array('fields_defaults' => array('total_amount', 'trxn_id', 'receive_date', 'id', 'contribution_status_id')))
+      + $this->getColumns('LineItem', array('fields_defaults' => array('financial_type_id', 'line_total', 'tax_amount', 'contribution_id')));
     parent::__construct();
   }
 
@@ -61,7 +60,7 @@ class CRM_Financialaudit_Form_Report_Financialaudit extends CRM_Extendedreport_F
   /**
    * Select from clauses to use.
    *
-   * (from those advertised using $this->getAvailableJoins()).
+   * (from ExtendedReports).
    *
    * @return array
    */
@@ -78,6 +77,7 @@ class CRM_Financialaudit_Form_Report_Financialaudit extends CRM_Extendedreport_F
 
   function orderBy() {
     parent::orderBy();
+    $this->_groupByArray = ['civicrm_contribution_id' => $this->_aliases['civicrm_contribution'] . '.id'];
   }
 
   /**
@@ -101,6 +101,11 @@ class CRM_Financialaudit_Form_Report_Financialaudit extends CRM_Extendedreport_F
   public function alterDisplay(&$rows) {
     parent::alterDisplay($rows);
 
+    // when we Group by Contribution ID => the total_amount_count means => Number of LineItems
+    // change label:
+    $this->_columnHeaders['civicrm_contribution_contribution_total_amount_count']['title'] = "#Line Items";
+    $this->_columnHeaders['civicrm_line_item_line_item_contribution_id_count']['title'] = "Delta with Line Items";
+
     foreach ($rows as $rowNum => $row) {
 
       if (array_key_exists('civicrm_contact_civicrm_contact_sort_name', $row) &&
@@ -118,6 +123,19 @@ class CRM_Financialaudit_Form_Report_Financialaudit extends CRM_Extendedreport_F
 
       $rows[$rowNum]['civicrm_contribution_contribution_total_amount_sum'] = $rows[$rowNum]['civicrm_contribution_contribution_total_amount_sum'] / $rows[$rowNum]['civicrm_contribution_contribution_total_amount_count'];
 
+      $rows[$rowNum]['civicrm_line_item_line_item_contribution_id_count'] = $rows[$rowNum]['civicrm_contribution_contribution_total_amount_sum'] - ($rows[$rowNum]['civicrm_line_item_line_item_line_total_sum'] + $rows[$rowNum]['civicrm_line_item_line_item_tax_amount_sum']);
+
+      // civicrm/contact/view/contribution?reset=1&id=4406&cid=2&action=view&context=contribution&selectedChild=contribute
+      if (abs($rows[$rowNum]['civicrm_line_item_line_item_contribution_id_count']) > 0.001) {
+        $url = CRM_Utils_System::url('civicrm/contact/view',
+          'reset=1&id=' . $row['civicrm_contribution_contribution_id'] . '&cid=' . $row['civicrm_contact_civicrm_contact_contact_id'],
+          $this->_absoluteUrl
+        );
+        $rows[$rowNum]['civicrm_contribution_contribution_id_link'] = $url;
+        $rows[$rowNum]['civicrm_contribution_contribution_id_hover'] = ts('View Contribution.');
+        $rows[$rowNum]['civicrm_line_item_line_item_contribution_id_count_link'] = $url;
+        $rows[$rowNum]['civicrm_line_item_line_item_contribution_id_count_hover'] = ts('View Contribution.');
+      }
     }
 
   }
